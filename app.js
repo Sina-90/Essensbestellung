338712c27fdcba12ee46f409e4f042f449a23f18
Sina-90 <s.kleiner@ruetten-stb.de>
2026-07-02 15:21:21 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://sbkuqafnxptdibsuqtfc.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNia3VxYWZueHB0ZGlic3VxdGZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI5ODQ3ODIsImV4cCI6MjA5ODU2MDc4Mn0.PUpjutvvbQo5H2G9kaHUhbxGnJ6GmrnvhRWYMh9ZUYc";
+const SUPABASE_KEY = "DEIN_KEY_HIER";
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
